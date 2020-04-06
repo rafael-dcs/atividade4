f7acb4d598a2231f4e0355291f2ba0978f1d0e87
rafael-dcs <rafael.dcs@unijui.edu.br>
2020-04-06 16:54:31 -0300

    contact

---
+++ admin/about.php
@@ -0,0 +1,20 @@
+<section class="section">
+    <h2>About us</h2>
+    <form action="" id="about">
+        <div class="form-group">
+            <label for="title">Title</label>
+            <input class="form-control" type="text" name="title">
+        </div>
+        <div class="form-group">
+            <label for="title">About us</label><br>
+            <textarea name="about" cols="60" rows="8"></textarea>
+        </div>
+        <div class="form-group">
+            <label for="title">Image</label>
+            <input type="file">
+        </div>
+        <div class="edit">
+            <button class="btn btn-default">Edit</button>
+        </div>
+    </form>
+</section>
---
+++ admin/contact.php
@@ -0,0 +1,32 @@
+<section class="section">
+    <h2>Contact</h2>
+    <form action="">
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="text" name="email" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="phone">Telephone</label>
+            <input type="text" name="phone" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="face">Facebook</label>
+            <input type="text" name="face" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="address">Address</label>
+            <input type="text" name="address" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="district">District</label>
+            <input type="text" name="district" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="city">City</label>
+            <input type="text" name="city" class="form-control">
+        </div>
+        <div class="edit">
+            <button class="btn btn-default">Edit</button>
+        </div>
+    </form>
+</section>
